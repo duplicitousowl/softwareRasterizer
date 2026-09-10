02fa9cf74dfa3abc942c4efcec691028abcde446
NightOwl <duplicitousowl@gmail.com>
2026-09-09 23:37:33 -0500

    add 3d rotation, projection, and perspective

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ struct Shape {
 };
 
 // Define a rotate function that takes in a vertex, applies the rotation formula, and returns a new vertex to preserve original
-Vertex rotate_vertex(Vertex v, float theta) {
+Vertex rotate_z(Vertex v, float theta) {
     float xp = (v.x * std::cos(theta)) - (v.y * std::sin(theta));
     float yp = (v.x * std::sin(theta)) + (v.y * std::cos(theta));
 
@@ -40,18 +40,51 @@ Vertex rotate_vertex(Vertex v, float theta) {
     return rotated_vertex;
 }
 
+Vertex rotate_x (Vertex v, float theta) {
+    float yp = (v.y * std::cos(theta)) - (v.z * std::sin(theta));
+    float zp = (v.y * std::sin(theta)) + (v.z * std::cos(theta));
+
+    Vertex rotated_vertex = {v.x, yp, zp};
+    return rotated_vertex;
+}
+
+Vertex rotate_y(Vertex v, float theta) {
+    float zp = (v.z * std::cos(theta)) - (v.x * std::sin(theta));
+    float xp = (v.z * std::sin(theta)) + (v.x * std::cos(theta));
+
+    Vertex rotated_vertex = {xp, v.y, zp};
+    return rotated_vertex;
+}
+
+Vertex rotate3d(Vertex v, float theta_x, float theta_y, float theta_z){
+    return rotate_x(rotate_y(rotate_z(v, theta_z), theta_y), theta_x);
+}
+
 // Define a rotate shape function that loops over a shapes vertices, runs them through the rotate vertex function and returns a new rotated shape.
-Shape rotate_shape (Shape shape, float theta) {
+Shape rotate_shape(Shape shape, float theta_x, float theta_y, float theta_z) {
     Shape shape_to_return;
     shape_to_return.edges = shape.edges;
 
     for (int i = 0; i < shape.vertices.size(); ++i) {
         Vertex rotated_vertex;
-        rotated_vertex = rotate_vertex(shape.vertices[i], theta);
+        rotated_vertex = rotate3d(shape.vertices[i], theta_x, theta_y, theta_z);
         shape_to_return.vertices.push_back(rotated_vertex);
     }
 
     return shape_to_return;
+}
+
+Vertex project_vertex (Vertex v, float d) {
+    float scale = d / (v.z + d);
+    return {v.x * scale, v.y * scale, v.z};
+
+}
+
+Shape project_shape(Shape shape, float d) {
+    for (int i = 0; i < shape.vertices.size(); ++i){
+        shape.vertices[i] = project_vertex(shape.vertices[i], d);
+    }
+    return shape;
 }
 
 
@@ -126,34 +159,24 @@ void draw_shape(Shape shape, uint32_t color, std::vector<uint32_t>& pixels) {
 Shape make_test_box() {
     Shape s;
     s.vertices = {
-        {-100, 100},
-        {-100, -100},
-        {100, -100},
-        {100, 100}
+        {-100, 100, -100},
+        {-100, -100, -100},
+        {100, -100, -100},
+        {100, 100, -100},
+        {-100, 100, 100},
+        {-100, -100, 100},
+        {100, -100, 100},
+        {100, 100, 100}
     };
     s.edges = {
-        {0,1}, {1,2}, {2,3}, {3,0}
+        {0,1}, {1,2}, {2,3}, {3,0}, {4,5}, {5,6}, {6,7},{7,4},{0,4},{1,5},{2,6},{3,7}
     };
     return s;
 }
 
-void draw_box(Point p1, uint32_t color, std::vector<uint32_t>& pixels) {
-    Point cart_tl = {-p1.x, p1.y};
-    Point cart_tr = {p1.x, p1.y};
-    Point cart_br = {p1.x, -p1.y};
-    Point cart_bl = {-p1.x, -p1.y};
 
-    Point sp1_tl = c_to_screen(cart_tl.x, cart_tl.y);
-    Point sp1_tr = c_to_screen(cart_tr.x, cart_tr.y);
-    Point sp1_br = c_to_screen(cart_br.x, cart_br.y);
-    Point sp1_bl = c_to_screen(cart_bl.x, cart_bl.y);
 
-    draw_line(sp1_tl.x, sp1_tl.y, sp1_tr.x, sp1_tr.y, color, pixels);
-    draw_line(sp1_tr.x, sp1_tr.y, sp1_br.x, sp1_br.y, color, pixels);
-    draw_line(sp1_br.x, sp1_br.y, sp1_bl.x, sp1_bl.y, color, pixels);
-    draw_line(sp1_bl.x, sp1_bl.y, sp1_tl.x, sp1_tl.y, color, pixels);
-}
-
+float theta = 0.00f;
 
 int main() {    
     // Initialize pixel vector (480,000 pixels)
@@ -200,12 +223,9 @@ int main() {
             std::fill(pixels.begin(), pixels.end(), 0xFFFFFFFF);
 
             uint32_t black = 0xFF000000;
-            float theta = 0.5f;
+            theta -= 0.001f;
 
-            Shape rotated_box = rotate_shape(test_box, theta);
-            
-
-            draw_shape(rotated_box, black, pixels);
+           draw_shape(project_shape(rotate_shape(test_box, theta, theta*.6f, theta*1.2f), 500), black, pixels);
 
             // clear the renderer in anticipation of filling it with the updated texture
             SDL_RenderClear(renderer);
